dff283f8425f18a6cf87fba2d69da979fd7ffdd6
ahmad capal <ahmadcapal@gmail.com>
2024-08-15 06:28:10 +0300

    additional chapter

---
+++ car/script.js
@@ -0,0 +1,14 @@
+const car = {
+  toyota: {
+    suv: 'fortuner',
+    coupe: '',
+    minivan: '',
+    sedan: '',
+
+    pickup: 'hilux',
+  },
+  hyundai: {
+    suv: 'fortuner',
+    pickup: 'hilux',
+  },
+};
