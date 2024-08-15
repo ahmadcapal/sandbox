4eff7c08cafbbc444710200064a0da9d6b65cd51
ahmad capal <ahmadcapal@gmail.com>
2024-08-15 09:03:03 +0300

    additional chapter

---
+++ library/script.js
@@ -0,0 +1,56 @@
+const library = [
+  {
+    title: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+    status: {
+      own: true,
+      reading: false,
+      read: false,
+    },
+  },
+  {
+    title: '1984',
+    author: 'George Orwell',
+    status: {
+      own: false,
+      reading: true,
+      read: false,
+    },
+  },
+  {
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    status: {
+      own: false,
+      reading: false,
+      read: true,
+    },
+  },
+  {
+    title: 'Moby Dick',
+    author: 'Herman Melville',
+    status: {
+      own: true,
+      reading: false,
+      read: false,
+    },
+  },
+  {
+    title: 'Pride and Prejudice',
+    author: 'Jane Austen',
+    status: {
+      own: false,
+      reading: true,
+      read: false,
+    },
+  },
+  {
+    title: 'The Catcher in the Rye',
+    author: 'J.D. Salinger',
+    status: {
+      own: false,
+      reading: false,
+      read: true,
+    },
+  },
+];
