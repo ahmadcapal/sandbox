043256d881b01aa9d3971e3b9d64b73ac84225b3
ahmad capal <ahmadcapal@gmail.com>
2024-08-18 21:35:39 +0300

    car json add

--- library/script.js
+++ library/script.js
@@ -1,56 +1,56 @@
 const library = [
   {
-    title: 'To Kill a Mockingbird',
-    author: 'Harper Lee',
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
     status: {
       own: true,
       reading: false,
-      read: false,
-    },
+      read: false
+    }
   },
   {
-    title: '1984',
-    author: 'George Orwell',
-    status: {
-      own: false,
-      reading: true,
-      read: false,
-    },
-  },
-  {
-    title: 'The Great Gatsby',
-    author: 'F. Scott Fitzgerald',
-    status: {
-      own: false,
-      reading: false,
-      read: true,
-    },
-  },
-  {
-    title: 'Moby Dick',
-    author: 'Herman Melville',
+    title: "1984",
+    author: "George Orwell",
     status: {
       own: true,
       reading: false,
-      read: false,
-    },
+      read: false
+    }
   },
   {
-    title: 'Pride and Prejudice',
-    author: 'Jane Austen',
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
     status: {
-      own: false,
-      reading: true,
-      read: false,
-    },
-  },
-  {
-    title: 'The Catcher in the Rye',
-    author: 'J.D. Salinger',
-    status: {
-      own: false,
+      own: true,
       reading: false,
-      read: true,
-    },
+      read: false
+    }
   },
+  {
+    title: "Moby Dick",
+    author: "Herman Melville",
+    status: {
+      own: true,
+      reading: false,
+      read: false
+    }
+  },
+  {
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+    status: {
+      own: true,
+      reading: false,
+      read: false
+    }
+  },
+  {
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    status: {
+      own: true,
+      reading: false,
+      read: false
+    }
+  }
 ];
